a9a2f8a0b1126296d97ec3e9a51b089a0609a9c2
Amadeu Alexandre <amadeu.alexandre87@gmail.com>
2026-04-13 16:19:07 -0300

    style: rename sidebar communities menu and link to explore

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -96,9 +96,9 @@
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 12V8H6a2 2 0 0 1-2-2c0-1.1.9-2 2-2h12v4"/><path d="M4 6v12c0 1.1.9 2 2 2h14v-4"/><path d="M18 12a2 2 0 0 0-2 2c0 1.1.9 2 2 2h4v-4h-4z"/></svg>
                 Carteira
             </a>
-            <a href="{{ route('communities.my') }}" class="menu-item sidebar-nav-item {{ Route::is('communities.my') ? 'active' : '' }}">
+            <a href="{{ route('communities.explore') }}" class="menu-item sidebar-nav-item {{ Route::is('communities.explore') ? 'active' : '' }}">
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M23 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
-                Minhas Comunidades
+                Comunidades
             </a>
             
             <div class="sidebar-divider mobile-only"></div>
