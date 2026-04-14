0c7445b9202dafbbe75330e2f137db45ffc3efd4
Amadeu Alexandre <amadeu.alexandre87@gmail.com>
2026-04-14 11:25:12 -0300

    Fix Chat menu alignment in Feed: remove redundant inline margin

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -90,7 +90,7 @@
                         </span>
                     @endif
                 </div>
-                <span style="margin-left: 12px;">Chat</span>
+                <span>Chat</span>
             </a>
             <a href="{{ route('purchases.index') }}" class="menu-item sidebar-nav-item {{ Route::is('purchases.*') ? 'active' : '' }}">
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z"/><path d="M3 6h18"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
